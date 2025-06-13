5fb477fd01d62fba111d60f06ac7cd99de176272
vanlam304 <111721629+vanlam304@users.noreply.github.com>
2025-06-13 11:03:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo "🌐 Cài đặt UPNET Hosting Manager..."
+echo "📦 Cài đặt curl, nếu chưa có..."
 apt update -y && apt install -y curl
 
-curl -sSL https://raw.githubusercontent.com/vanlam304/upnet-hosting-manager/main/auto.sh -o /usr/local/bin/upnet
+echo "⬇️  Đang tải UPNET Hosting Manager..."
+curl -sSL https://raw.githubusercontent.com/vanlam304/upnet-hosting-manager/main/Vps%20Hosting%20Manager -o /usr/local/bin/upnet
 chmod +x /usr/local/bin/upnet
 
-echo "✅ Cài đặt xong. Dùng lệnh 'upnet' để mở menu quản lý VPS."
+echo "✅ Hoàn tất! Gõ 'upnet' để mở trình quản lý."
